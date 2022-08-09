a8841af6a488ea0a181a67340ce6b499c0a70cc7
Leonardo <hgtleohgt@gmail.com>
2022-08-09 17:56:59 -0300

    Exercicios Aula 13 Leonardo Henrique

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,17 @@
-const num = //seu numero aqui
+// 1. Utilizando ifs aninhados
+let num = Number(prompt("Digite um número para saber se e divisivel por 2 e 3"))
 
+if (num%2 == 0){
+    if(num%3 == 0){
+        console.log("Ifs aninhados - Divisivel por 2 por 3")
+    }
+}else{
+    console.log("Ifs aninhados - Não divisivel")
+}
+
+// 2. Utilizando um operador lógico para unir duas operações relacionais
+if (num % 2 == 0 && num % 3 == 0){
+    console.log("Operadores lógicos - Divisivel")
+}else{
+    console.log("Operadores lógicos - Não divisivel")
+}
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,7 @@
-//código a ser reescrito
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
-} else {
-	console.log("Olá, "+nome);
-}
+let nome = prompt("Olá, qual o seu nome?")
+nome == "Jose" ? console.log("Oi, Zé!"):console.log("Ola", nome)
 
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
-} else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+let idade = Number(prompt("Qual a sua idade?"))
+idade >= 18 ? console.log("Pode tirar carteira de motorista"): console.log("Ainda não pode tirar carteira de motorista")
+
